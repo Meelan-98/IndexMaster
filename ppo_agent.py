af9ea6110c2db01eeecc992d1b80d2866a518af4
Meelan-98 <meelanbandara@gmail.com>
2023-07-16 11:00:21 +0530

    inferencing added

--- ppo_agent.py
+++ ppo_agent.py
@@ -5,7 +5,7 @@ import numpy as np
 import csv
 from load_wise.environment import initial_state_function,state_change, get_action
 from load_wise.reward_cal import get_reward
-
+import configparser
 
 
 def export_to_csv(data, filename):
@@ -14,12 +14,15 @@ def export_to_csv(data, filename):
         csv_writer.writerows(data)
 
 class MongoDBIndexSelectionEnv(gym.Env):
-    def __init__(self, initial_state_fn, reward_fn, state_change_fn, w_path):
+    def __init__(self, initial_state_fn, reward_fn, state_change_fn, config_file):
 
         self.action_space = spaces.Discrete(3) 
         self.observation_space = spaces.MultiBinary(10)
 
-        self.workload_path = w_path
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
+        self.workload_path = None
         self.rem_queries = 0
         self.tot_queries = 0
         
@@ -27,12 +30,22 @@ class MongoDBIndexSelectionEnv(gym.Env):
         self.reward_fn = reward_fn
         self.state_change_fn = state_change_fn
         
+        self.inference = False
         self.state = None
         self.reset()
         
     def reset(self):
-        
-        state_data = self.initial_state_fn(self.workload_path)
+
+        if (self.inference == True):
+            print("Resetting to Inference")
+            workload_loc = str(self.config.get("env", "test_state"))
+            self.workload_path = workload_loc
+        else:
+            print("Resetting to Training")
+            workload_loc = str(self.config.get("env", "train_state"))
+            self.workload_path = workload_loc
+
+        state_data = self.initial_state_fn(workload_loc)
         self.state = state_data[0]
         self.rem_queries = state_data[1]
         self.tot_queries = state_data[1]
@@ -55,17 +68,24 @@ class MongoDBIndexSelectionEnv(gym.Env):
             self.state = self.state_change_fn(self.workload_path,self.tot_queries,self.rem_queries)  # Change the state
         
         return np.array(self.state), reward, done, {"time" : rewards[0]}
+    
+    def enter_inference(self):
+        self.inference = True
+
+    def exit_inference(self):
+        self.inference = False
 
 workload = "train_workload"
 workload_path = "workloads/" + workload + ".json"
 
-env = MongoDBIndexSelectionEnv(initial_state_function, get_reward, state_change, workload_path)
+env = MongoDBIndexSelectionEnv(initial_state_function, get_reward, state_change, "config.ini")
 
 print("Started Learning........")
 model = PPO("MlpPolicy", env, verbose=2)
 model.learn(total_timesteps=10000)
 
 print("Learning done ! Entering prediction mode")
+env.enter_inference()
 obs = env.reset()
 done = False
 
