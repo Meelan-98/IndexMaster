964856b1d23e08081a8cb4ba9c4fb92f5023988e
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-07-16 19:27:56 +0530

    Change index

--- data-generator-tool/index_creator.py
+++ data-generator-tool/index_creator.py
@@ -13,8 +13,12 @@ collection = db['TestCatalog']
 
 collection.create_index("phone_number")
 collection.create_index("email")
+collection.create_index("first_name")
+collection.create_index("last_name")
+collection.create_index("job_title")
+collection.create_index("company")
+collection.create_index("title")
 collection.create_index([("first_name", 1), ("last_name", 1)])
 collection.create_index([("job_title", 1), ("company", 1)])
-collection.create_index([("title", 1), ("first_name", 1)])
 
 client.close()
